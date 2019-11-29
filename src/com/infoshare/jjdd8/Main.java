72a1bc0725772433cb1f769ab71427c82e05972d
Chris <ichris.kk@gmail.com>
2019-11-29 11:21:19 +0100

    simple table

---
+++ src/com/infoshare/jjdd8/Main.java
@@ -0,0 +1,9 @@
+package com.infoshare.jjdd8;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[] sqaure = {0, 1, 2, 4, 9, 12, 25, 36, 49};
+        System.out.println(sqaure[8]);
+    }
+}
