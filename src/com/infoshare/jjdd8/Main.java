87f418e810062e3bfe70ee4d60db2fd1d1b2fb20
Chris <ichris.kk@gmail.com>
2019-11-29 12:13:24 +0100

    simple table

--- src/com/infoshare/jjdd8/Main.java
+++ src/com/infoshare/jjdd8/Main.java
@@ -1,9 +1,50 @@
 package com.infoshare.jjdd8;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-	int[] sqaure = {0, 1, 2, 4, 9, 12, 25, 36, 49};
-        System.out.println(sqaure[8]);
+
+
+    public static int[] getSquares(int size) {
+        int[] squares = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            squares[i] = getSquare(i);
+        }
+        return squares;
     }
-}
+
+    private static int getSquare(int i) {
+        return i * i;
+    }
+
+    public static void main(String[] args) {
+        int[] square = {0, 1, 2, 4, 9, 12, 25, 36, 49};
+        System.out.println(square[8]);
+
+        int[] squares = getSquares(5);
+
+        System.out.println(Arrays.toString(squares));
+    }
+
+    };
+
+    /**
+    public static int[] getSquare(int size) {
+        int[] squares = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            squares[i] = getSquare(i);
+        }
+        return squares;
+    }
+
+    private static int getSquare(int i){
+        return i*i;
+    };
+     **/
+
+
